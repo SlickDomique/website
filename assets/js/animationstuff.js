0125557dfdd3bce7d1aa4f32ede7c53157994e8f
domi <slickdomique@proton.me>
2024-02-18 17:41:57 +0100

    Updating scripts, cleaning up home scripts

--- assets/js/animationstuff.js
+++ assets/js/animationstuff.js
@@ -1,69 +1,72 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 const mouse = {
-    x: 0,
-    y: 0
-  };
-(function() {
-    const loader = new GLTFLoader();
+  x: 0,
+  y: 0,
+};
+(function () {
+  if (window.location.pathname !== "/") return;
 
-    // Optional: Provide a DRACOLoader instance to decode compressed mesh data
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath( '/examples/jsm/libs/draco/' );
-    loader.setDRACOLoader( dracoLoader );
+  const loader = new GLTFLoader();
 
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
+  // Optional: Provide a DRACOLoader instance to decode compressed mesh data
+  const dracoLoader = new DRACOLoader();
+  dracoLoader.setDecoderPath("/examples/jsm/libs/draco/");
+  loader.setDRACOLoader(dracoLoader);
 
-    const dingusPlace = document.getElementById("dingusPlace");
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
 
-    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    renderer.setSize( 500, 500 );
-    dingusPlace.appendChild(renderer.domElement);
-    camera.position.z = 35;
-    camera.position.y = 20;
-    camera.rotation.x = -0.4;
-    let dingus = null;
+  const dingusPlace = document.getElementById("dingusPlace");
 
-    const lightAmb = new THREE.AmbientLight(0xffffff);
-    scene.add(lightAmb);
+  const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(500, 500);
+  dingusPlace.appendChild(renderer.domElement);
+  camera.position.z = 35;
+  camera.position.y = 20;
+  camera.rotation.x = -0.4;
+  let dingus = null;
 
-    document.addEventListener('mousemove', onMouseMove, false);
-    function onMouseMove(event) {
-        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    };
+  const lightAmb = new THREE.AmbientLight(0xffffff);
+  scene.add(lightAmb);
 
+  document.addEventListener("mousemove", onMouseMove, false);
+  function onMouseMove(event) {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  }
 
-    function animate() {
-        requestAnimationFrame( animate );
+  function animate() {
+    requestAnimationFrame(animate);
 
-        if (dingus) {
-            dingus.rotation.y += mouse.x * 0.05;
-            console.log(dingus.rotation.x)
-            if (dingus.rotation.x + mouse.y * 0.001 > -0.4 && dingus.rotation.x + mouse.y * 0.001 < 0.1) {
-                dingus.rotation.x += mouse.y * 0.001;
-            }
-        }
-
-        renderer.render( scene, camera );
+    if (dingus) {
+      dingus.rotation.y += mouse.x * 0.05;
+      if (
+        dingus.rotation.x + mouse.y * 0.001 > -0.4 &&
+        dingus.rotation.x + mouse.y * 0.001 < 0.1
+      ) {
+        dingus.rotation.x += mouse.y * 0.001;
+      }
     }
-    animate();
 
-    loader.load(
-        // resource URL
-        'assets/dingus_the_cat.glb',
-        // called when the resource is loaded
-        function ( gltf ) {
-            dingus = gltf.scene;
-            scene.add( gltf.scene );
+    renderer.render(scene, camera);
+  }
+  animate();
 
-            gltf.animations; // Array<THREE.AnimationClip>
-            gltf.scene; // THREE.Group
-            gltf.scenes; // Array<THREE.Group>
-            gltf.cameras; // Array<THREE.Camera>
-            gltf.asset; // Object
-        }
-    );
+  loader.load(
+    // resource URL
+    "assets/dingus_the_cat.glb",
+    // called when the resource is loaded
+    function (gltf) {
+      dingus = gltf.scene;
+      scene.add(gltf.scene);
+
+      gltf.animations; // Array<THREE.AnimationClip>
+      gltf.scene; // THREE.Group
+      gltf.scenes; // Array<THREE.Group>
+      gltf.cameras; // Array<THREE.Camera>
+      gltf.asset; // Object
+    }
+  );
 })();
